2a2f360720f1586677d6e3f50ec2ec942a41f393
Nagaraju <varthalanagaraju@gmail.com>
2026-01-01 13:21:42 +0530

    Initial commit

---
+++ HP Trivia/HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Nagaraju on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
